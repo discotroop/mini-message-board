9f03aab22685bc888ef62b324d9eddb87f63d2b9
John Mark Goodson <john.mark.hat@gmail.com>
2020-06-13 13:47:10 -0400

    new router points to form view properly

--- routes/new.js
+++ routes/new.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/new', function(req, res, next) {
-  res.render('form');
+router.get('/', function(req, res, next) {
+  res.render('form', {title: "new message" });
 });
 
 module.exports = router;
